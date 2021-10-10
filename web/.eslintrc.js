86ff3db6ebc2ba1f7621d2988eb31b67fa2f6a5a
Shubhad Mathur <32930066+frontseat-astronaut@users.noreply.github.com>
2021-10-10 19:26:52 +0530

    [web] add flow annotation in .eslintrc.js

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,13 +1,11 @@
+// @flow
+
 module.exports = {
-    "env": {
-        "browser": true,
-        "node": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        require.resolve('eslint-config-fusion'),
-    ],
-    "rules": {
-    }
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', require.resolve('eslint-config-fusion')],
+  rules: {},
 };
